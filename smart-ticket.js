f462e013db9e157255f8a20fa73b61b46e4453b7
Nasif-Haider <nasifhaider.me@gmail.com>
2024-02-21 19:57:49 +0600

    Final commit

--- smart-ticket.js
+++ smart-ticket.js
@@ -1,44 +1,48 @@
-// function getSeatById(elementId){
-//     const element = document.getElementById(elementId);
-//     element.classList.add('bg-[#1DD100]');
-//     element.classList.remove('bg-[#F7F8F8]');
-
-// }
-
-
-
-// let seatArray = [];
-// function selectSeat(elementId){
-//     getSeatById(elementId);
-
-//     let selectedSeat = document.getElementById(elementId).innerText;
-//     let totalSelectedSeat = selectedSeat + 1;
-//     seatArray.push(totalSelectedSeat);
-
-
-
-// }
-
-// const totalSeat = getConvertedValue("total-seat");
-// const totalSelectedSeat = getConvertedValue("total-selected-seat");
+function seeAllOffers (){
+    document.getElementById("transitTrove-section").scrollIntoView({behavior: 'smooth'});
+}
 
 
 const allSeat = document.getElementsByClassName("click-btn");
 
+
 for (const seat of allSeat) {
     seat.addEventListener("click", function (event) {
         const seatNumberText = event.target.innerText;
-        // console.log(seatNumberText, "Economy class", "500");
-        
+        console.log(seatNumberText);
+
         const TicketPrice = '550';
         const price = parseInt(TicketPrice);
-        // console.log(convertedPrice);
 
-        const selectedSeatContainer = document.getElementById("selected-player-container");
+        const selectedSeatContainer = document.getElementById("selected-seat-container");
+
+        // event.target.setAttribute("disabled", false);        
+
+        const seatCount = getConvertedValue("total-selected-seat");
+        if (seatCount + 1 > 4) {
+            alert("You can't buy more than 4 tickets at a time.");
+            return;
+        }
+
+        const element = document.getElementById(seatNumberText);
+        element.classList.add('bg-[#1DD100]');
+        element.classList.remove('bg-[#F7F8F8]');
+        
+        event.target.setAttribute("disabled", false);  
+
+        // Update seat left
+        const seatLeft = getConvertedValue("total-seat");
+        document.getElementById("total-seat").innerText = seatLeft - 1;
+
+        // Update selected seat 
+        const selectSeat = getConvertedValue("total-selected-seat");
+        document.getElementById("total-selected-seat").innerText = selectSeat + 1;
+
+
         const div = document.createElement("div");
         div.classList.add("flex");
         div.classList.add("justify-around");
-        
+
         const p1 = document.createElement("p");
         const p2 = document.createElement("p");
         const p3 = document.createElement("p");
@@ -54,23 +58,43 @@ for (const seat of allSeat) {
         selectedSeatContainer.appendChild(div);
 
         updateTotalPrice(price);
+
+        updateGrandTotal();
     })
 }
 
-function updateTotalPrice (value){
+
+function updateGrandTotal(status) {
+    const totalPrice = getConvertedValue("total-price");
+    if (status == undefined) {
+        document.getElementById("grand-total").innerText = totalPrice;
+    }
+    else {
+        const couponCode01 = document.getElementById("first-coupon").innerText;
+        const couponCode02 = document.getElementById("second-coupon").innerText;
+        const userInputCoupon = document.getElementById("userInput-coupon").value;
+        if (couponCode01 == userInputCoupon) {
+            const discount = totalPrice * 0.15;
+            document.getElementById("grand-total").innerText = totalPrice - discount;
+        }
+        else if (couponCode02 == userInputCoupon) {
+            const discount = totalPrice * 0.2;
+            document.getElementById("grand-total").innerText = totalPrice - discount;
+        }
+        else {
+            alert("Please enter valid coupon code");
+        }
+    }
+
+}
+
+
+function updateTotalPrice(value) {
     const totalCost = getConvertedValue("total-price");
     const sum = totalCost + value;
     // console.log(totalCost, sum);
     document.getElementById("total-price").innerText = sum;
 }
-
-
-
-
-
-
-
-
 
 
 function getConvertedValue(id) {
